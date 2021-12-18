035cc9efb70b4a02afe5f99c6ca426ae4ba9782f
jay <dj.min43@gmail.com>
2021-12-18 17:49:56 +0900

    chore: turn on decorator

--- class-and-interface/classAndInterface.js
+++ class-and-interface/classAndInterface.js
@@ -1,7 +1,26 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.testerFunction = void 0;
+var StringDatabase = /** @class */ (function () {
+    function StringDatabase() {
+        this.state = {};
+    }
+    StringDatabase.prototype.get = function (key) {
+        return key in this.state ? this.state[key] : null;
+    };
+    StringDatabase.prototype.set = function (key, value) {
+        this.state[key] = value;
+    };
+    StringDatabase.from = function (state) {
+        var db = new StringDatabase();
+        for (var key in state) {
+            db.set(key, state[key]);
+        }
+        return db;
+    };
+    return StringDatabase;
+}());
 var testerFunction = function () {
-    console.log("this is a tester function");
+    console.log(typeof StringDatabase);
 };
 exports.testerFunction = testerFunction;
