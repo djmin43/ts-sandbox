dcf30da86e208d3fe19249878ba0cd514ba525cf
james <james.min@freedgrouptech.com>
2021-12-22 07:00:21 +0900

    chore: remove my name func

--- generic/generic.ts
+++ generic/generic.ts
@@ -20,13 +20,3 @@ const getNameTwo: GetNameTwo<string> = (value) => {
 const getNameThree: GetNameThree = (value) => {
   return value;
 };
-
-const mapNames = <T, U>(array: T[], f: (item: T) => U): U[] => {
-  let result = [];
-  for (let i = 0; i < array.length; i++) {
-    result[i] = f(array(i));
-  }
-  return result;
-};
-
-getNameOne(myName, count);
