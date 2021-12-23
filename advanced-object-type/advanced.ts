92bf63816c5e1b51b5de6400e1da10511ecf55de
james <james.min@freedgrouptech.com>
2021-12-23 09:29:19 +0900

    .

--- advanced-object-type/advanced.ts
+++ advanced-object-type/advanced.ts
@@ -11,6 +11,24 @@ type APIResponse = {
   };
 };
 
-type Friend = APIResponse["user"]["friendList"];
+type FriendList = "friendList";
+
+type Friend = APIResponse["user"][FriendList];
 
 type Two = Friend["friends"][0];
+
+type ResponseKeys = keyof APIResponse["user"];
+
+// Mapping Types
+
+type Account = {
+  id: number;
+  isEmployee: boolean;
+  notes: string[];
+};
+
+type OptionalAccount = {
+  [K in keyof Account]: Account[K];
+};
+
+type AccountKey = [keyof Account];
