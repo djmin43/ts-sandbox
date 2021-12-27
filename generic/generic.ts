8fbbcb80efe2ab629757e7abf5af99af19cb2014
jay <dj.min43@gmail.com>
2021-12-28 08:37:37 +0900

    chore: add comment about two different generic types

--- generic/generic.ts
+++ generic/generic.ts
@@ -1,17 +1,23 @@
 const arrayify = <T extends unknown>(value: T): Array<T> => [value];
 
+// each generic type is assigned to each call signature.
+// eacg function has its own generic type
 type ArrayFunctionOne = {
   <T>(value: T): [T];
+  <U>(value: U): U;
 };
-
-type ArrayFunctionTwo<T> = {
-  (value: T): [T];
-};
-
 const arrayFunctionerOne: ArrayFunctionOne = (value) => {
   return [value];
 };
 
+const arrayFunctionerThree: ArrayFunctionOne = (value) => {
+  return value;
+};
+
+// on the other hand, this generic is assinged to all. that being said, generic should be assigned with type.
+type ArrayFunctionTwo<T> = {
+  (value: T): [T];
+};
 const arrayFunctionerTwo: ArrayFunctionTwo<string> = (value) => {
   return [value];
 };
