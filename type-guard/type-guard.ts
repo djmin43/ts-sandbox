18c4c57bce9cbe84a192f4bdc93617fecd63ae1b
james <james.min@freedgrouptech.com>
2021-12-29 07:04:16 +0900

    typeguard for type of

---
+++ type-guard/type-guard.ts
@@ -0,0 +1,12 @@
+// type guard can narrow down types at if statements
+
+// typescript knows about JS's substring prototype.
+function doSomething(x: number | string) {
+  if (typeof x === "string") {
+    console.log(x.substring(1));
+    // typescript tells the user substing does not exist in string prototype
+    console.log(x.substing(1));
+  }
+  // this throws an error because it is not guaranteed that x is string
+  x.substring(1);
+}
