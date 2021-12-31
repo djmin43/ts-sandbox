f633b2aacf575c731eec23aba40512f6e1fb38e6
jay <dj.min43@gmail.com>
2021-12-31 09:31:01 +0900

    chore

--- type-guard/type-guard.js
+++ type-guard/type-guard.js
@@ -2,12 +2,12 @@
 // type guard can narrow down types at if statements
 // typescript knows about JS's substring prototype.
 function doSomething(x) {
-  if (typeof x === "string") {
-    console.log(x.substring(1));
-    // typescript tells the user substing does not exist in string prototype
-    // console.log(x.substing(1));
-  }
-  // this throws an error because it is not guaranteed that x is string
-  // x.substring(1);
+    if (typeof x === "string") {
+        console.log(x.substring(1));
+        // typescript tells the user substing does not exist in string prototype
+        console.log(x.substing(1));
+    }
+    // this throws an error because it is not guaranteed that x is string
+    x.substring(1);
 }
 // 사용자 정의 Type Guard 함수란 단순히 어떤 인자명은 어떠한 타입이다라는 값을 리턴하는 함수일 뿐입니다.
