95a1efeee76219a26372c329cf9611962f592798
jay <dj.min43@gmail.com>
2022-01-01 09:58:28 +0900

    chore: write some code for testing out duck typing

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 "use strict";
+// duck typing
+function calculateLength(v) {
+    return Math.sqrt(v.x * v.x + v.y * +v.y);
+}
---
+++ index.ts
@@ -0,0 +1,16 @@
+// duck typing
+
+interface Vector2D {
+  x: number
+  y: number
+}
+
+function calculateLength(v: Vector2D) {
+  return Math.sqrt(v.x * v.x + v.y * + v.y)
+}
+
+interface NamedVector {
+  name: string
+  x: number
+  y: number
+}
