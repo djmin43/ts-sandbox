1fed7d58cf4d217bfacc12090d1202150c0dd3f6
jay <dj.min43@gmail.com>
2022-01-02 14:26:36 +0900

    feat: infer classes

--- infer/infer.js
+++ infer/infer.js
@@ -2,3 +2,23 @@
 // INFER
 // typescript infers x is number
 var x = 3;
+var Rhino = /** @class */ (function () {
+    function Rhino() {
+        this.type = 'animal';
+    }
+    return Rhino;
+}());
+var Elephant = /** @class */ (function () {
+    function Elephant() {
+        this.type = 'animal';
+    }
+    return Elephant;
+}());
+var Snake = /** @class */ (function () {
+    function Snake() {
+        this.type = 'animal';
+    }
+    return Snake;
+}());
+// this infers type
+var zoo = [new Rhino(), new Elephant(), new Snake()];
--- infer/infer.ts
+++ infer/infer.ts
@@ -1,3 +1,18 @@
 // INFER
 // typescript infers x is number
 let x = 3
+
+class Rhino {
+  type = 'animal'
+}
+
+class Elephant {
+  type = 'animal'
+}
+
+class Snake {
+  type = 'animal'
+}
+
+// this infers type
+let zoo = [new Rhino(), new Elephant(), new Snake()]
