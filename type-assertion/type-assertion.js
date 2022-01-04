22f927df1cb76bceea48437669cf6f202476773c
james <james.min@freedgrouptech.com>
2022-01-05 07:05:03 +0900

    feat: add type assertion file

---
+++ type-assertion/type-assertion.js
@@ -0,0 +1,2 @@
+"use strict";
+// One note to consider -> a type should on be asserted by its super-type or sub-type
---
+++ type-assertion/type-assertion.ts
@@ -0,0 +1,1 @@
+// One note to consider -> a type should on be asserted by its super-type or sub-type
