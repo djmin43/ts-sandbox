32f0f0807f75eb7bc62b632c712927f4b3b94ab3
james <james.min@freedgrouptech.com>
2022-01-05 07:11:59 +0900

    feat: note about type assertion

--- type-assertion/type-assertion.js
+++ type-assertion/type-assertion.js
@@ -1,2 +1,16 @@
 "use strict";
+// 1. Type Assertion
 // One note to consider -> a type should on be asserted by its super-type or sub-type
+// for example, number cannot assert string
+function formatInput(input) {
+    return input;
+}
+function getUserInput() {
+    return 'user';
+}
+var input = getUserInput();
+formatInput(input);
+formatInput(input);
+// same as 'as'
+// this is not recommended to avoid confusion with TSX syntax.
+formatInput(input);
--- type-assertion/type-assertion.ts
+++ type-assertion/type-assertion.ts
@@ -1,1 +1,23 @@
+
+// 1. Type Assertion
+
 // One note to consider -> a type should on be asserted by its super-type or sub-type
+// for example, number cannot assert string
+
+function formatInput(input: string) {
+  return input
+}
+
+function getUserInput(): string | number {
+  return 'user'
+}
+
+let input = getUserInput()
+
+
+formatInput(input as string)
+formatInput(input as 'hello')
+
+// same as 'as'
+// this is not recommended to avoid confusion with TSX syntax.
+formatInput(<string>input)
