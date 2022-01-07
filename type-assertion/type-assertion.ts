02d43abf893150ed68a355cfab1a47425d0a012b
jay <dj.min43@gmail.com>
2022-01-07 15:08:06 +0900

    chore: fix warning

--- type-assertion/type-assertion.ts
+++ type-assertion/type-assertion.ts
@@ -37,7 +37,7 @@ function closeDialog(dialog: Dialog) {
   setTimeout(() =>
     removeFromDOM(
       dialog,
-      document.getElementById(dialog.id)!
+      document.getElementById(dialog.id!)!
     )
   )
 }
